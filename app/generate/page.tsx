8834d8d6401b6a03edd7ab017e42ed857448ad2d
Mathurah <mathurah25@gmail.com>
2024-03-23 10:28:48 -0400

    created text area and button

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,4 +1,15 @@
 import Image from "next/image";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <h1>Hello, Generate page!</h1>;
+  return (
+    <div className="p-6 gap-6 flex flex-col items-center justify-center place-content-center">
+      <div className="text-2xl font-bold">phrame ai 🖼️</div>
+      <div className="container max-w-xl">
+        <Textarea placeholder="Type a prompt for your artwork here" />
+      </div>
+      <Button>Generate</Button>
+    </div>
+  );
 }
