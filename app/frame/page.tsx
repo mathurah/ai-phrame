c637142e213b43069a7a306ad948c2fbc71cbc92
Mathurah <mathurah25@gmail.com>
2024-03-23 10:08:12 -0400

    created the initial routes

---
+++ app/frame/page.tsx
@@ -0,0 +1,4 @@
+import Image from "next/image";
+export default function Page() {
+  return <h1>Hello, frame page!</h1>;
+}
---
+++ app/generate/page.tsx
@@ -0,0 +1,4 @@
+import Image from "next/image";
+export default function Page() {
+  return <h1>Hello, Generate page!</h1>;
+}
